8621dfc471e7739bb2eabf8f2fc4383bb67463b7
saidiadem <saidiadem@insat.u-carthage.tn>
2024-09-21 13:36:34 +0100

    trying to resolve errors

--- next.config.mjs
+++ next.config.mjs
@@ -3,15 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   async headers() {
     return [
-      {
-        source: '/resume.pdf',
-        headers: [
-          {
-            key: 'Content-Type',
-            value: 'application/pdf',
-          },
-        ],
-      },
+      
     ];
   },
 };
