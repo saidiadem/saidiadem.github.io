727580f4f8e1e2df512e2c7f669c83c3e783bf6c
saidiadem <saidiadem@insat.u-carthage.tn>
2024-09-21 13:17:35 +0100

    deployment changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: '/saidiadem.github.io',
-  assetPrefix: '/saidiadem.github.io',
-  output:'export',
   async headers() {
     return [
       {
